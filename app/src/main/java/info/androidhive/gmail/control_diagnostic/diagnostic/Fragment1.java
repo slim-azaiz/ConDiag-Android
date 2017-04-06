e5e420fa9199818bee79ad35a12d39136db53baf
slim0993 <slim.azaiz@ensi-uma.tn>
2017-04-06 17:53:18 +0100

    realtime not fixed

--- app/src/main/java/info/androidhive/gmail/control_diagnostic/diagnostic/Fragment1.java
+++ app/src/main/java/info/androidhive/gmail/control_diagnostic/diagnostic/Fragment1.java
@@ -6,6 +6,7 @@ package info.androidhive.gmail.control_diagnostic.diagnostic;
 
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -14,19 +15,29 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.Executors;
+
 import in.srain.cube.views.ptr.PtrClassicFrameLayout;
+import in.srain.cube.views.ptr.PtrDefaultHandler;
+import in.srain.cube.views.ptr.PtrFrameLayout;
+import in.srain.cube.views.ptr.PtrHandler;
 import info.androidhive.gmail.R;
 import info.androidhive.gmail.adapter.DiagnosticAdapter;
 import info.androidhive.gmail.adapter.ServerAdapter;
+import info.androidhive.gmail.discovery.dial.ServerFinder;
 import info.androidhive.gmail.helper.DividerItemDecoration;
 import info.androidhive.gmail.model.Diagnostic;
 import info.androidhive.gmail.network.ApiClient;
 import info.androidhive.gmail.network.ApiInterface;
 import info.androidhive.gmail.network.JSONResponse;
 import info.androidhive.gmail.network.RequestInterface;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -35,12 +46,12 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 
 public class Fragment1 extends Fragment   {
-    //public static PtrClassicFrameLayout mPtrFrame;
+    private  PtrClassicFrameLayout mPtrFrame;
     private RecyclerView recyclerView;
     private ArrayList<Diagnostic> data;
     private DiagnosticAdapter adapter;
-
-
+    private  OkHttpClient client;
+    private  Handler handler;
     public Fragment1() {
 
     }
@@ -48,12 +59,69 @@ public class Fragment1 extends Fragment   {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view= inflater.inflate(R.layout.fragment_1, container, false);
+        mPtrFrame = (PtrClassicFrameLayout) view.findViewById(R.id.rotate_header_list_view_frameDiag);
+
+
         recyclerView = (RecyclerView) view.findViewById(R.id.recycler_viewDiag);
-      //  mPtrFrame = (PtrClassicFrameLayout) view.findViewById(R.id.rotate_header_list_view_frameDiag);
         recyclerView.setHasFixedSize(true);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
         recyclerView.setLayoutManager(layoutManager);
-        loadJSON();
+
+
+        //loadJSON();
+        mPtrFrame = (PtrClassicFrameLayout) view.findViewById(R.id.rotate_header_list_view_frameDiag);
+        mPtrFrame.setLastUpdateTimeRelateObject(this);
+        mPtrFrame.setPtrHandler(new PtrHandler() {
+            @Override
+            public void onRefreshBegin(PtrFrameLayout frame) {
+                    loadJSON();
+
+
+
+               // adapter.notifyDataSetChanged();
+                handler = new Handler();
+
+                handler.postDelayed(new Runnable(){
+                    public void run(){
+                        loadJSON();
+                        handler.postDelayed(this, 2000);
+                    }
+                }, 2000);
+
+
+
+            }
+
+            @Override
+            public boolean checkCanDoRefresh(PtrFrameLayout frame, View content, View header) {
+                return PtrDefaultHandler.checkContentCanBePulledDown(frame, content, header);
+            }
+        });
+        // the following are default settings
+        mPtrFrame.setResistance(1.7f);
+        mPtrFrame.setRatioOfHeaderHeightToRefresh(1.2f);
+        mPtrFrame.setDurationToClose(200);
+        mPtrFrame.setDurationToCloseHeader(1000);
+        // default is false
+        mPtrFrame.setPullToRefresh(false);
+        // default is true
+        mPtrFrame.setKeepHeaderWhenRefresh(true);
+        mPtrFrame.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mPtrFrame.autoRefresh();
+            }
+        }, 100);
+
+        /*client = new OkHttpClient();
+        client.interceptors().add(new Interceptor() {
+            @Override
+            public okhttp3.Response intercept(Chain chain) throws IOException {
+                okhttp3.Response response = chain.proceed(chain.request());
+                // Log.i("RESPONSE",response.toString());
+                return response;
+            }
+        });*/
         return  view;
     }
 
@@ -61,6 +129,7 @@ public class Fragment1 extends Fragment   {
 
 
     private void loadJSON(){
+
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("http://10.206.208.98:8000")
                 .addConverterFactory(GsonConverterFactory.create())
@@ -75,12 +144,18 @@ public class Fragment1 extends Fragment   {
                 data = new ArrayList<>(Arrays.asList(jsonResponse.getDiagnostics()));
                 adapter = new DiagnosticAdapter(data);
                 recyclerView.setAdapter(adapter);
+                mPtrFrame.refreshComplete();
             }
             @Override
             public void onFailure(Call<JSONResponse> call, Throwable t) {
+                mPtrFrame.refreshComplete();
+                Toast.makeText(getContext(), "Unable to fetch json: " + t.getMessage(), Toast.LENGTH_LONG).show();
                 Log.d("Error",t.getMessage());
+
             }
         });
+
+
 
     }
 
--- app/src/main/java/info/androidhive/gmail/network/RequestInterface.java
+++ app/src/main/java/info/androidhive/gmail/network/RequestInterface.java
@@ -2,9 +2,11 @@ package info.androidhive.gmail.network;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Streaming;
 
 public interface RequestInterface {
 
     @GET("/diag")
+    @Streaming
     Call<JSONResponse> getJSON();
 }
